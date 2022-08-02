e3dd08aaf9b54e2d8222639c4f58ba7c0f36502c
bahrihrf34 <bahrihrf34@gmail.com>
2022-08-03 00:40:22 +0300

    env load

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/bahri-hirfanoglu/go-socketio/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	fmt.Println("Server Running...")
 	server.CreateServer()
 
